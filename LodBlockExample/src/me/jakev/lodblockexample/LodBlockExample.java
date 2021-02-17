1bee50e242b6224f07b9433f9504496067920390
Jake <jakevvolkenburg@gmail.com>
2021-02-17 07:35:48 -0700

    Update lowmade

--- LodBlockExample/src/me/jakev/lodblockexample/LodBlockExample.java
+++ LodBlockExample/src/me/jakev/lodblockexample/LodBlockExample.java
@@ -23,20 +23,22 @@ public class LodBlockExample extends StarMod {
     public void onBlockConfigLoad(BlockConfig config) {
         ElementInformation chair = config.newElement(this, "le chair", new short[]{195});
         BlockConfig.setBasicInfo(chair, "le chair", 100, 0.1F, true, true, 78);
-        config.assignLod(chair, this, "cube", null);
+        config.assignLod(chair, this, "EpicSlope", null);
         config.add(chair);
     }
 
 
     @Override
     public void onEnable() {
+
+        System.err.println("Called OnEnable of LodBlockExample");
         StarLoaderTexture.runOnGraphicsThread(new Runnable() {
             @Override
             public void run() {
                 try {
                     System.err.println("Loading lodblockexam");
                     GameResourceLoader resLoader = (GameResourceLoader) Controller.getResLoader();
-                    resLoader.getMeshLoader().loadModMesh(LodBlockExample.this, "cube", getJarResource("me/jakev/lodblockexample/cube.zip"), "Console");
+                    resLoader.getMeshLoader().loadModMesh(LodBlockExample.this, "EpicSlope", getJarResource("me/jakev/lodblockexample/EpicSlope.zip"), "Console");
                 } catch (ResourceException | IOException e) {
                     e.printStackTrace();
                 }
--- LowMade/src/me/jakev/lowmade/LowMade.java
+++ LowMade/src/me/jakev/lowmade/LowMade.java
@@ -5,17 +5,6 @@ import api.mod.config.FileConfiguration;
 
 public class LowMade extends StarMod {
     public static LowMade inst;
-
-    public static void main(String[] args) { }
-    @Override
-    public void onGameStart() {
-        inst = this;
-        setModName("LowMade");
-        setModVersion("1.2");
-        setModDescription("Loads far away textures in 1x1 res");
-        setModAuthor("JakeV");
-        setModSMVersion("0.202.108");
-    }
     public static int resolution = 1;
     @Override
     public void onEnable() {
